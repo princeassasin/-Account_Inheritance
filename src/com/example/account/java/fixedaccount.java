ee09c8801cef1b05945f84b3d089ef294ca01af3
princeassasin <sriramlakshmim@gmial.com>
2017-05-04 12:31:36 +0530

    fixed error

--- src/com/example/account/java/fixedaccount.java
+++ src/com/example/account/java/fixedaccount.java
@@ -4,14 +4,14 @@ package com.example.account.java;
 public class fixedaccount extends account {
 
 	double Ammount;
-	double cal1;
+	//double cal1;
 	double cal;
 	public void calculate(){
 		
 		super.getDetail();
-		cal1 = (1+ super.annual /super.numtime)*cal;
+		//cal1 = (1+ super.annual /super.numtime)*cal;
 		cal=Math.exp(super.numtime*super.year);
-		Ammount= super.principle*cal1;
+		Ammount= super.principle*(1+ super.annual /super.numtime)*cal;
 		System.out.println(Ammount);
 	}
 	
